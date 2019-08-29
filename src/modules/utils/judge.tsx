5a97e1c0d806604bdb3a29e2f85e14f4777e15d5
Sugitasan <r0815cramg@gmail.com>
2019-08-29 13:09:05 +0900

    canTsumoagari 完成

--- src/modules/utils/judge.tsx
+++ src/modules/utils/judge.tsx
@@ -1,8 +1,7 @@
-
 class Judge {
     category: string[];
     chiPattern: { [ key: number ]: number[][] };
-
+  
     constructor() {
         this.category = ['m', 'p', 's', 'z']
         this.chiPattern = {
@@ -17,11 +16,11 @@ class Judge {
             8 : [ [6,7] ],
         };
     }
-
+  
     toituCount(tehai: string[]) {
         // let toitu = tehai.filter((v, i, self) => self.indexOf(v) !== i);
     }
-
+  
     // TODO
     // 鳴き・上がり判定
     canNaki(tehai: string[], suteHai: string, from: number) {
@@ -29,6 +28,104 @@ class Judge {
         // return this.canPon(tehai, suteHai) || this.canDaiMinkan(tehai, suteHai) || this.canChi(tehai, suteHai, from);
     }
     canTsumoAgari(tehai: string[], agariHai: string) {
+        let idx_tehai:number[] = Array(38).fill(0);
+        let tmp_idx_tehai:number[];
+        let idx:number;
+  
+        const Reset_tmp = (arr:number[],i:number) => {
+            let tmp_arr = arr.concat();
+            tmp_arr[i] -= 2;
+            return tmp_arr;
+        };
+        const Check_shuntsu = (arr:number[]) => {
+            for (let i = 1; i < 38 - 10; i++){
+                if (i % 10 > 7) continue;
+                while(arr[i] > 0 && arr[i + 1] > 0 && arr[i + 2] > 0) {
+                    arr[i]--; arr[i + 1]--; arr[i + 2]--;
+                }
+            }
+            return arr;
+        };
+        const Check_kotsu = (arr:number[]) => { 
+            return arr.map( v => v % 3);
+         };
+        const Check_7toitsu = (arr:number[]) => {
+            for (let i = 1; i < 38; i++){
+                if (arr[i] > 1)
+                    arr[i] %= 2;
+            }
+            if (arr.every(v => v === 0))
+                return true;
+            else
+                return false;
+        };
+        const Check_kokushi = (arr:number[]) => {
+            let atama = false;
+            for (let i = 1; i < 38; i++) {
+                switch(i) {
+                case 1: case 9: case 11: case 19: case 21: case 29: 
+                case 31: case 32: case 33: case 34: case 35: case 36: case 37:
+                    if (!atama && arr[i] == 2) {
+                        arr[i] -= 2;
+                        atama = true;
+                    } else if (arr[i] == 1) {
+                        arr[i] -= 1;
+                    }
+                    break;
+                default:
+                    break;
+                }
+            }
+            if (arr.every(v => v === 0))
+                return true;
+            else
+                return false;
+        }
+  
+        tehai.forEach(v => {
+            idx = parseInt(v.slice(-1)) + ( vv => {
+                switch(vv.slice(0,1)) {
+                case "s":
+                    return 10;
+                case "p":
+                    return 20;
+                case "z":
+                    return 30;
+                case "m":
+                default :
+                    return 0;
+                }
+            })(v);
+            idx_tehai[idx] += 1;
+        });
+        
+        for (let i = 0; i < 38; i++){
+            if (idx_tehai[i] >= 2) {
+  
+                //順子抜き→刻子抜き
+                tmp_idx_tehai = Reset_tmp(idx_tehai,i);
+                tmp_idx_tehai = Check_shuntsu(tmp_idx_tehai);
+                tmp_idx_tehai = Check_kotsu(tmp_idx_tehai);
+                if (tmp_idx_tehai.every(v => v === 0)) 
+                    return true;
+                
+                //刻子抜き→順子抜き
+                tmp_idx_tehai = Reset_tmp(idx_tehai,i);               
+                tmp_idx_tehai = Check_kotsu(tmp_idx_tehai);
+                tmp_idx_tehai = Check_shuntsu(tmp_idx_tehai);
+                if (tmp_idx_tehai.every(v => v === 0)) 
+                    return true;
+  
+                //七対子
+                tmp_idx_tehai = Reset_tmp(idx_tehai,i);
+                if (Check_7toitsu(tmp_idx_tehai)) 
+                    return true;
+  
+                //国士無双
+                if (Check_kokushi(idx_tehai)) 
+                    return true;
+            }
+        }
         return false;
     }
     canRonAgari(tehai: string[], agariHai: string, from: number) {
@@ -41,15 +138,16 @@ class Judge {
         return tehai.filter((v) => v = suteHai).length = 3;
     }
     canChi(tehai: string[], suteHai: string, from: number){
-        if (from !== 3) return false;
+        if (from !== 3) return false; //上家じゃなかったら
         let ret = [];
         const sutehaiCategory = suteHai.slice(0,1);
-        const sutehaiNum = +suteHai.slice(1);
-        let tmp = tehai.filter((v) => v.slice(0,1) === sutehaiCategory).map(str => parseInt(str, 10));
+        //if (sutehaiCategory === 'z') return false;
+        const sutehaiNum = +suteHai.slice(1);       
+        let tmp = tehai.filter((v) => v.slice(0,1) === sutehaiCategory).map(str => parseInt(str, 10)); 
         console.log(tmp);
         if(this.chiPattern[sutehaiNum]) {
             this.chiPattern[sutehaiNum].forEach(v => {
-                if(tmp.includes(v[0])&&tmp.includes(v[0])) ret.push(v);
+                if(tmp.includes(v[0])&&tmp.includes(v[0])) ret.push(v); 
             });
         }
         return ret.length > 0
@@ -61,7 +159,7 @@ class Judge {
         return (tehai.filter((v) => v = kanHai).length = 1)
             && (ponHai.filter((v) => v = kanHai).length = 3);
     }
-
+  
     // 役判定
     isRichi(tehai: string[], agariHai: string) {
         return true;
@@ -87,12 +185,12 @@ class Judge {
     isSanshokuDoujun(tehai: string[], agariHai: string) {
         return true;
     }
-
-}
-
-// debug
-let judge = new Judge();
-let a = judge.canChi(['s1','s2'], 's3', 3);
-console.log(a);
-
-export default Judge;
+  
+  }
+  
+  // debug
+  let judge = new Judge();
+  let a = judge.canChi(['s1','s2'], 's3', 3);
+  console.log(a);
+  
+  export default Judge;
